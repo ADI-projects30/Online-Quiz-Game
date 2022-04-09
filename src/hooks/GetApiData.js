1f8d4324e6a18730793fb8f3fc858d0773ba507c
ADI-projects30 <adidas2712@gmail.com>
2022-04-09 19:24:08 +0300

    fixing

--- src/hooks/GetApiData.js
+++ src/hooks/GetApiData.js
@@ -7,16 +7,12 @@ axios.defaults.baseURL = "https://opentdb.com";
 const GetApiData =  ({ url }) => {
     const [response, setResponse] = useState({});
     const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
-       
-    const [ sub, setSub] = useState({"trivia_categories":[{enterr: 'Entertaiment'}]});
-  
+    const [loading, setLoading] = useState(true);  
     useEffect(() => {
       const fetchData = async() => {
         await axios
           .get(url)
           .then((res) => setResponse(res.data))
-          //  .then(setSub({"trivia_categories":[{enterr: 'Entertaiment'}]}))
           .catch((err) => setError(err))
           .finally(() => setLoading(false));
       };
